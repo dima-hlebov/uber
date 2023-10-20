1e63f544723b652c86af5d562fb7b5fd024d35e8
Dmytro <glebov.dymitriy@gmail.com>
2023-10-20 20:10:55 +0300

    fix: update folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,32 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
 // Static server
-gulp.task('server', function() {
+gulp.task('server', function () {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "/"
         }
     });
 });
 
-gulp.task('styles', function(){
-    return gulp.src('src/sass/*.+(scss|sass)')
-            .pipe(sass({outputStyle: 'compressed'})).on('error', sass.logError)
-            .pipe(rename({
-                    suffix: ".min"
-                }))
-            .pipe(autoprefixer({
-                    browsers: ['last 2 versions'],
-                    cascade: false
-                }))
-            .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(gulp.dest("src/css"))
-            .pipe(browserSync.stream());
+gulp.task('styles', function () {
+    return gulp.src('sass/*.+(scss|sass)')
+        .pipe(sass({ outputStyle: 'compressed' })).on('error', sass.logError)
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function(){
-    gulp.watch('src/sass/*.+(scss|sass)', gulp.parallel('styles'));
-    gulp.watch('src/*.html').on('change', browserSync.reload);
+gulp.task('watch', function () {
+    gulp.watch('sass/*.+(scss|sass)', gulp.parallel('styles'));
+    gulp.watch('*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
